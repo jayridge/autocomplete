3a2b11406201a182fff63eef9443f6f87cf60bc3
Nathan Folkman <nathan.folkman@gmail.com>
2012-09-27 17:37:58 -0700

    Fix warning related to unitialized evtimer_del().

--- autocomplete.c
+++ autocomplete.c
@@ -238,8 +238,10 @@ void backup(int timer_fd, short event, void *arg)
         uint32_t when;
         uint32_t count;
     } hdr;
-    
-    evtimer_del(&backup_timer);
+
+    if (evtimer_initialized(&backup_timer)) {
+        evtimer_del(&backup_timer);
+    }
     evtimer_set(&backup_timer, backup, NULL);
     evtimer_add(&backup_timer, &backup_tv);
 
